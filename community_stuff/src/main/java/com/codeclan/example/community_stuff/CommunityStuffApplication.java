51d3499e195665065564e886ae5098238c2b2f03
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-04-08 09:57:32 +0100

    Initial commit

---
+++ community_stuff/src/main/java/com/codeclan/example/community_stuff/CommunityStuffApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.community_stuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityStuffApplication.class, args);
+	}
+
+}
---
+++ community_stuff/src/test/java/com/codeclan/example/community_stuff/CommunityStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.community_stuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
