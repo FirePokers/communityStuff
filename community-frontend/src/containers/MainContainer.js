c0fec570c373054ef0cfc005f13c81dd8a5e9ffe
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-04-09 14:55:00 +0100

    fixed maincontainer initial state setting

--- community-frontend/src/containers/MainContainer.js
+++ community-frontend/src/containers/MainContainer.js
@@ -4,18 +4,36 @@ import Request from '../helpers/request';
 
 const MainContainer = () => {
 
-   const [currentUser, setCurrentUser] = useEffect(null);
+   const [currentUser, setCurrentUser] = useState(null);
+   const [allAssets, setAllAssets] = useState(null);
+   const [filterTags, setFilterTags] = useState([]);
+   const [chosenAsset, setChosenAsset] = useState(null);
 
-   const request = new Request();
+   const requestAll = function(){
+        
+        const request = new Request();
+        const userPromise = request.get('/api/users/1')
+        const assetPromise = request.get('.api/assets')
 
-   Promise.all(
-       [
-           request.get('api/users/1')
-       ]
-   )
-   .then((data) => {setCurrentUser(data)});
+        Promise.all([userPromise, assetPromise])
+        .then((data) => {
+            setCurrentUser(data[0]);
+            setAllAssets(data[1]);
+        })
+
+   }
+
+   useEffect(() => {
+       requestAll();
+   }, []);
+
+   return (
+       <p>I am the maincontainer</p>
+   ) 
+
+    
    
-   
+
 }
 
 export default MainContainer;
