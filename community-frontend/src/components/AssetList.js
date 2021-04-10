56bdcd95d0e3df5b592e1e8426ddcae7d7546b0c
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2021-04-10 01:14:05 +0100

    inventory page structure update

---
+++ community-frontend/src/components/AssetList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AssetList = () => {
+
+
+    return (
+        <div className="inventory-leftpanel panel dissolve-appear3">
+            <h1>Tools appear here</h1>
+
+            <p>This will be multiple boxes containing different tools. They will take up the bulk of the page.</p>
+
+        </div>
+    );
+
+
+};
+
+export default AssetList;
--- community-frontend/src/components/Inventory.js
+++ community-frontend/src/components/Inventory.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import AssetList from './AssetList';
 import '../css/inventory.css';
 
 
@@ -8,19 +9,25 @@ const Inventory = ({allAssets, allTags}) => {
 
     return (
         <>
-            <div className="inventory-searchbox panel in-from-right">
+            <div className="inventory-top-row">
+                <div className="inventory-searchbox panel in-from-right">
+                    <form>
+                    <i className="fas fa-search"></i>
+                        <input type="text" id="searchTerm" name="searchTerm" className="panel-text"/>
+                    </form>
 
+                </div>
             </div>
-            <main>
+            <div className="inventory-centre-row">
                 <div className="inventory-leftpanel panel in-from-left">
                     <h1>Side Panel Header</h1>
 
                     <p>Some paragraph text goes in here</p>
 
                 </div>
-                
+               <AssetList /> 
 
-            </main>
+            </div>
         </>
     )
 
