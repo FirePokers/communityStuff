15fb9e0efe831128ecaa0f60c2e4910877fe45a1
CodeScott00 <dougal_00@hotmail.com>
2021-04-11 16:23:51 +0100

    some css added and delete user working

--- community-frontend/src/components/EditForm.js
+++ community-frontend/src/components/EditForm.js
@@ -45,7 +45,7 @@ const Editform = ({user, onEdit, onDelete}) => {
                 <input type="text" placeholder="User Name" name="userName" onChange={handleChange} value={stateUser.userName} />
                 <input type="text" placeholder="First Name" name="firstName" onChange={handleChange} value={stateUser.firstName} />
                 <input type="text" placeholder="Last Name" name="lastName" onChange={handleChange} value={stateUser.lastName} />
-                <input type="email" placeholder="Email" name="email" onChange={handleChange} value={stateUser.email} />
+                <input type="text" placeholder="Email" name="email" onChange={handleChange} value={stateUser.email} />
 
                 <button type="submit">Edit</button>
 
